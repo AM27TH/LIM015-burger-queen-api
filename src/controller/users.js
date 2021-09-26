afbe173bf25ead46bd1e63872a2d8d87bc7a4fc5
Anghel Melanie Trillo Hernández <amelanie.trillo27@gmail.com>
2021-09-25 22:33:53 -0500

    Controlador de usuarios completo

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 const {
-  emailValidation, passwordValidation,
+  emailValidation, passwordValidation, idValidation,
 } = require('../services/validation');
 
 module.exports = {
@@ -14,9 +14,13 @@ module.exports = {
   },
   getUserById: async (req, resp, next) => {
     try {
+      // obtener token de verificación para validar si es admin
+      const userToken = await User.findById(req.authToken.id);
       const user = await User.findById(req.params.uid);
       if (!user) return next(404);
-      resp.status(200).json(user);
+      // Si no es admin o el mismo usuario
+      if (!userToken.roles.admin && !(userToken.email === user.email)) return next(403);
+      return resp.json(user);
     } catch (error) {
       return next(error);
     }
@@ -38,14 +42,76 @@ module.exports = {
         roles: ((!roles) || !(roles.admin)) ? { admin: false } : { admin: true },
       });
       // guardamos el usuario
-      await newUser.save();
-      return resp.status(201).json({ message: 'Usuario creado' });
+      const savedUser = await newUser.save();
+      return resp.json(savedUser);
     } catch (error) {
       return next(error);
     }
   },
-  updateUserById: (req, resp) => {
+  updateUserById: async (req, resp, next) => {
+    try {
+      // obtener token de verificación para validar si es admin
+      const userToken = await User.findById(req.authToken.id);
+      // Datos de usuario a modificar
+      const { uid } = req.params;
+      let user = null;
+      // Validar si recibimos uid o email
+      if (idValidation(uid)) { user = await User.findById(uid); }
+      if (emailValidation(uid)) { user = await User.findOne({ email: uid }); }
+      if (!user) return next(404);
+      // Validar cuerpo de request
+      if (!Object.keys(req.body).length) return next(400);
+      let { email, password, roles } = req.body;
+      // Si no es admin o el mismo usuario
+      if (!userToken.roles.admin && !(userToken.email === user.email)) return next(403);
+      if (!email) email = user.email;
+      if (!password) password = user.password;
+      if (!roles) roles = user.roles;
+      // Validación roles
+      if (user.roles !== roles) {
+        if (!userToken.roles.admin && !user.roles.admin) return next(403);
+        // Si usuario es admin e intenta modificar su rol
+        user.roles = ((roles.admin)) ? { admin: true } : { admin: false };
+      }
+      // Validación email
+      if (user.email !== email) {
+        if (!emailValidation(email)) return resp.status(400).json({ message: 'Por favor, ingrese un email válido' });
+        // Validamos si el correo ya esta registrado
+        const searchUser = await User.findOne({ email });
+        if (searchUser) return resp.status(403).json({ message: 'El email ya está registrado' });
+        // Reasignamos el email
+        user.email = email;
+      }
+      // Validación contraseña
+      if (!(await User.matchPassword(password, user.password))) {
+        if (!passwordValidation(password)) return resp.status(400).json({ message: 'Por favor, ingrese una contraseña válida' });
+        // Reasignamos contraseña
+        user.password = await User.encryptPassword(password);
+      }
+      // guardamos el usuario
+      const updatedUser = await user.save();
+      return resp.json(updatedUser);
+    } catch (error) {
+      return next(error);
+    }
   },
-  deleteUserById: (req, resp) => {
+  deleteUserById: async (req, resp, next) => {
+    try {
+      // obtener token de verificación para validar si es admin
+      const userToken = await User.findById(req.authToken.id);
+      // Datos de usuario a modificar
+      const { uid } = req.params;
+      let user = null;
+      // Validar si recibimos uid o email
+      if (idValidation(uid)) { user = await User.findById(uid); }
+      if (emailValidation(uid)) { user = await User.findOne({ email: uid }); }
+      if (!user) return next(404);
+      // Si no es admin o el mismo usuario
+      if (!userToken.roles.admin && !(userToken.email === user.email)) return next(403);
+      await User.findByIdAndDelete(user.id);
+      return resp.json(user);
+    } catch (error) {
+      return next(error);
+    }
   },
 };
