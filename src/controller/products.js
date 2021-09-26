1a01f9cdacd70e38fd29df40fe3bad6d39baa0c8
Anghel Melanie Trillo Hernández <amelanie.trillo27@gmail.com>
2021-09-25 22:35:45 -0500

    Controlador de productos

--- src/controller/products.js
+++ src/controller/products.js
@@ -11,9 +11,9 @@ module.exports = {
   },
   getProductById: async (req, resp, next) => {
     try {
-      const product = await Product.findById(req.params.productId);
+      const product = await Product.findOne({ _id: req.params.productId });
       if (!product) return next(404);
-      resp.status(200).json(product);
+      return resp.json(product);
     } catch (error) {
       return next(error);
     }
@@ -23,6 +23,10 @@ module.exports = {
       const {
         name, type, price, image,
       } = req.body;
+      // Validar si existe el producto
+      const product = await Product.findOne({ name });
+      if (product) return resp.status(404).json({ message: 'Producto ya existe' });
+      // Validar campos name y price
       if (name === '' && price === '') return next(400);
       const newProduct = new Product({
         name, type, price, image,
@@ -35,20 +39,29 @@ module.exports = {
   },
   updateProductById: async (req, resp, next) => {
     try {
-      if (Object.entries(req.params.body).length === 0) return next(400);
-      const updateProduct = await Product.findByIdAndUpdate(req.params.productId, req.body,
-        { new: true });
-      if (!updateProduct) return next(404);
-      return resp.status(200).json(updateProduct);
+      // Validar si existe el producto
+      const product = await Product.findOne({ _id: req.params.productId });
+      if (!product) return next(404);
+      if (!Object.keys(req.body).length) return next(400);
+      const { name } = req.body;
+      // Validar si nombre nuevo ya existe
+      if (name) {
+        const productName = await Product.findOne({ name });
+        if (productName) return resp.status(400).json({ message: 'Ese nombre de producto ya existe' });
+      }
+      const updatedProduct = await Product.findByIdAndUpdate(req.params.productId, req.body, {
+        new: true,
+      });
+      return resp.json(updatedProduct);
     } catch (error) {
       return next(error);
     }
   },
   deleteProductById: async (req, resp, next) => {
     try {
-      const product = await Product.findByIdAndDelete(req.params.productId);
+      const product = await Product.findOneAndDelete({ _id: req.params.productId });
       if (!product) return next(404);
-      return resp.status(204).json(product);
+      return resp.json(product);
     } catch (error) {
       return next(error);
     }
