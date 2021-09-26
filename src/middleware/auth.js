073e2b8cd5cd166c9389e9ea98c3ed617ef9149b
Anghel Melanie Trillo Hernández <amelanie.trillo27@gmail.com>
2021-09-25 22:30:32 -0500

    Arreglando funciones de middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,36 +1,44 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const config = require('../config');
 
-module.exports = (secret) => (req, resp, next) => {
+module.exports = () => (req, resp, next) => {
+  const { secret } = config;
   const { authorization } = req.headers;
-
   if (!authorization) {
     return next();
   }
-
   const [type, token] = authorization.split(' ');
-
   if (type.toLowerCase() !== 'bearer') {
     return next();
   }
-
   jwt.verify(token, secret, (err, decodedToken) => {
     if (err) {
       return next(403);
     }
-
     // TODO: Verificar identidad del usuario usando `decodeToken.uid`
+    const user = User.findById(decodedToken.id);
+    user
+      .then((verifiedUser) => {
+        if (!verifiedUser) return resp.status(400).json({ message: 'Usuario no encontrado' });
+        req.authToken = decodedToken;
+        return next();
+      })
+      .catch(() => next(403));
   });
 };
 
-module.exports.isAuthenticated = (req) => (
-  // TODO: decidir por la informacion del request si la usuaria esta autenticada
-  false
-);
+module.exports.isAuthenticated = (req) => {
+  if (req.authToken) return true;
+  return false;
+};
 
-module.exports.isAdmin = (req) => (
+module.exports.isAdmin = async (req) => {
   // TODO: decidir por la informacion del request si la usuaria es admin
-  false
-);
+  const user = await User.findById(req.authToken.id);
+  if (user.roles.admin) return true;
+  return false;
+};
 
 module.exports.requireAuth = (req, resp, next) => (
   (!module.exports.isAuthenticated(req))
