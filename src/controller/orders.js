901c9343bc90969241b161f05bc1ee041dc54280
Anghel Melanie Trillo Hernández <amelanie.trillo27@gmail.com>
2021-09-25 22:36:44 -0500

    Controlador Orders, falta función addOrder y validacion de la misma

--- src/controller/orders.js
+++ src/controller/orders.js
@@ -1,14 +1,59 @@
-// const Order = require('../models/order');
+const Order = require('../models/order');
 
 module.exports = {
-  getOrders: async (req, res, next) => {
+  getOrders: async (req, resp, next) => {
+    try {
+      const orders = await Order.find();
+      return resp.json(orders);
+    } catch (error) {
+      next(error);
+    }
   },
-  getOrderById: async (req, res, next) => {
+  getOrderById: async (req, resp, next) => {
+    try {
+      const order = await Order.findOne({ _id: req.params.orderId });
+      if (!order) return next(404);
+      return resp.json(order);
+    } catch (error) {
+      return next(error);
+    }
   },
-  addOrder: async (req, res, next) => {
+  addOrder: async (req, resp, next) => {
+    try {
+      const { userId, client, products } = req.body;
+      if (!userId || !client || products.length === 0) return next(400);
+      const newOrder = new Order({
+        userId, client, products,
+      });
+      // TODO: Guardar newOrder
+      resp.json(newOrder);
+    } catch (error) {
+      next(error);
+    }
   },
-  updateOrderById: async (req, res, next) => {
+  updateOrderById: async (req, resp, next) => {
+    try {
+      // Validar si existe el producto
+      const order = await Order.findOne({ _id: req.params.orderId });
+      if (!order) return next(404);
+      if (!Object.keys(req.body).length) return next(400);
+      const { status } = req.body;
+      if (status !== 'pendin' || status !== 'canceled' || status !== 'delivering' || status !== 'delivered') return next(400);
+      const updatedOrder = await Order.findByIdAndUpdate(req.params.orderId, req.body, {
+        new: true,
+      });
+      return resp.json(updatedOrder);
+    } catch (error) {
+      return next(error);
+    }
   },
-  deleteOrderById: async (req, res, next) => {
+  deleteOrderById: async (req, resp, next) => {
+    try {
+      const order = await Order.findOneAndDelete({ _id: req.params.orderId });
+      if (!order) return next(404);
+      return resp.json(order);
+    } catch (error) {
+      return next(error);
+    }
   },
 };
