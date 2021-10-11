c3e0bcd7000bb7c8ec23998707ab902a00a27355
Anghel Melanie Trillo Hernández <amelanie.trillo27@gmail.com>
2021-10-11 09:25:49 -0500

    Agrega try catch a funciones

--- src/controller/orders.js
+++ src/controller/orders.js
@@ -3,66 +3,86 @@ const { idValidation } = require('../services/validation');
 const { paginate } = require('../services/pagination');
 
 module.exports = {
-  getOrders: async (req, resp) => {
-    const url = `${req.protocol}://${req.headers.host + req.path}`;
-    const limit = parseInt(req.query.limit, 10) || 10;
-    const page = parseInt(req.query.page, 10) || 1;
-    const orders = await Order.paginate({}, { limit, page, populate: 'products.product' });
-    resp.links(paginate(url, orders.limit, orders.page, orders.totalPages, orders));
-    return resp.json(orders.docs);
+  getOrders: async (req, resp, next) => {
+    try {
+      const url = `${req.protocol}://${req.headers.host + req.path}`;
+      const limit = parseInt(req.query.limit, 10) || 10;
+      const page = parseInt(req.query.page, 10) || 1;
+      const orders = await Order.paginate({}, { limit, page, populate: 'products.product' });
+      resp.links(paginate(url, orders.limit, orders.page, orders.totalPages, orders));
+      return resp.json(orders.docs);
+    } catch (error) {
+      next(error);
+    }
   },
   getOrderById: async (req, resp, next) => {
-    if (!idValidation(req.params.orderId)) return next(404);
-    const order = await Order.findOne({ _id: req.params.orderId }).populate('products.product');
-    if (!order) return next(404);
-    return resp.json(order);
+    try {
+      if (!idValidation(req.params.orderId)) return next(404);
+      const order = await Order.findOne({ _id: req.params.orderId }).populate('products.product');
+      if (!order) return next(404);
+      return resp.json(order);
+    } catch (error) {
+      next(error);
+    }
   },
   addOrder: async (req, resp, next) => {
-    const { userId, client, products } = req.body;
-    if (!userId || products.length === 0) return next(400);
-    const newOrder = new Order({
-      userId,
-      client,
-      products: products.map((product) => ({
-        qty: product.qty,
-        product: product.productId,
-      })),
-    });
-    await newOrder.save();
-    const currentOrder = await newOrder.populate('products.product');
-    resp.json(currentOrder);
+    try {
+      const { userId, client, products } = req.body;
+      if (!userId || products.length === 0) return next(400);
+      const newOrder = new Order({
+        userId,
+        client,
+        products: products.map((product) => ({
+          qty: product.qty,
+          product: product.productId,
+        })),
+      });
+      await newOrder.save();
+      const currentOrder = await newOrder.populate('products.product');
+      resp.json(currentOrder);
+    } catch (error) {
+      next(error);
+    }
   },
   updateOrderById: async (req, resp, next) => {
-    if (!idValidation(req.params.orderId)) return next(404);
-    // Validar si existe el producto
-    const order = await Order.findOne({ _id: req.params.orderId });
-    if (!order) return next(404);
-    if (!Object.keys(req.body).length) return next(400);
-    const {
-      userId, client, products, status,
-    } = req.body;
-    // Validación de status
-    const validStatus = ['pending', 'canceled', 'delivering', 'delivered', 'preparing'];
-    if (status) {
-      if (!validStatus.includes(status)) return resp.status(400).json({ message: 'Estado inválido' });
+    try {
+      if (!idValidation(req.params.orderId)) return next(404);
+      // Validar si existe el producto
+      const order = await Order.findOne({ _id: req.params.orderId });
+      if (!order) return next(404);
+      if (!Object.keys(req.body).length) return next(400);
+      const {
+        userId, client, products, status,
+      } = req.body;
+      // Validación de status
+      const validStatus = ['pending', 'canceled', 'delivering', 'delivered', 'preparing'];
+      if (status) {
+        if (!validStatus.includes(status)) return resp.status(400).json({ message: 'Estado inválido' });
+      }
+      if (status === 'delivered') order.dateProcessed = Date.now();
+      // Validación de otros campos
+      if (userId) order.userId = userId;
+      if (client) order.client = client;
+      if (status) order.status = status;
+      if (products) {
+        order.products = products.map((product) => ({
+          qty: product.qty, product: product.productId,
+        }));
+      }
+      await order.save();
+      return resp.json(order);
+    } catch (error) {
+      next(error);
     }
-    if (status === 'delivered') order.dateProcessed = Date.now();
-    // Validación de otros campos
-    if (userId) order.userId = userId;
-    if (client) order.client = client;
-    if (status) order.status = status;
-    if (products) {
-      order.products = products.map((product) => ({
-        qty: product.qty, product: product.productId,
-      }));
-    }
-    await order.save();
-    return resp.json(order);
   },
   deleteOrderById: async (req, resp, next) => {
-    if (!idValidation(req.params.orderId)) return next(404);
-    const order = await Order.findOneAndDelete({ _id: req.params.orderId });
-    if (!order) return next(404);
-    return resp.json(order);
+    try {
+      if (!idValidation(req.params.orderId)) return next(404);
+      const order = await Order.findOneAndDelete({ _id: req.params.orderId });
+      if (!order) return next(404);
+      return resp.json(order);
+    } catch (error) {
+      next(error);
+    }
   },
 };
--- src/controller/products.js
+++ src/controller/products.js
@@ -3,52 +3,72 @@ const { idValidation } = require('../services/validation');
 const { paginate } = require('../services/pagination');
 
 module.exports = {
-  getProducts: async (req, resp) => {
-    const url = `${req.protocol}://${req.headers.host + req.path}`;
-    const limit = parseInt(req.query.limit, 10) || 10;
-    const page = parseInt(req.query.page, 10) || 1;
-    const products = await Product.paginate({}, { limit, page });
-    resp.links(paginate(url, products.limit, products.page, products.totalPages, products));
-    return resp.json(products.docs);
+  getProducts: async (req, resp, next) => {
+    try {
+      const url = `${req.protocol}://${req.headers.host + req.path}`;
+      const limit = parseInt(req.query.limit, 10) || 10;
+      const page = parseInt(req.query.page, 10) || 1;
+      const products = await Product.paginate({}, { limit, page });
+      resp.links(paginate(url, products.limit, products.page, products.totalPages, products));
+      return resp.json(products.docs);
+    } catch (error) {
+      next(error);
+    }
   },
   getProductById: async (req, resp, next) => {
-    if (!idValidation(req.params.productId)) return next(404);
-    const product = await Product.findOne({ _id: req.params.productId });
-    if (!product) return next(404);
-    return resp.json(product);
+    try {
+      if (!idValidation(req.params.productId)) return next(404);
+      const product = await Product.findOne({ _id: req.params.productId });
+      if (!product) return next(404);
+      return resp.json(product);
+    } catch (error) {
+      next(error);
+    }
   },
   addProduct: async (req, resp, next) => {
-    const {
-      name, price, type, image,
-    } = req.body;
-    // Validar si existe el producto
-    const product = await Product.findOne({ name });
-    if (product) return resp.status(404).json({ message: 'Producto ya existe' });
-    // Validar campos name y price
-    if (!name || !price) return next(400);
-    const newProduct = new Product({
-      name, price, type, image,
-    });
-    const productSaved = await newProduct.save();
-    return resp.json(productSaved);
+    try {
+      const {
+        name, price, type, image,
+      } = req.body;
+      // Validar si existe el producto
+      const product = await Product.findOne({ name });
+      if (product) return resp.status(404).json({ message: 'Producto ya existe' });
+      // Validar campos name y price
+      if (!name || !price) return next(400);
+      const newProduct = new Product({
+        name, price, type, image,
+      });
+      const productSaved = await newProduct.save();
+      return resp.json(productSaved);
+    } catch (error) {
+      next(error);
+    }
   },
   updateProductById: async (req, resp, next) => {
-    if (!idValidation(req.params.productId)) return next(404);
-    // Validar si existe el producto
-    const product = await Product.findOne({ _id: req.params.productId });
-    if (!product) return next(404);
-    if (!Object.keys(req.body).length) return next(400);
-    const { price } = req.body;
-    if (price !== undefined && !parseFloat(price)) return next(400);
-    const updatedProduct = await Product.findByIdAndUpdate(req.params.productId, req.body, {
-      new: true,
-    });
-    return resp.json(updatedProduct);
+    try {
+      if (!idValidation(req.params.productId)) return next(404);
+      // Validar si existe el producto
+      const product = await Product.findOne({ _id: req.params.productId });
+      if (!product) return next(404);
+      if (!Object.keys(req.body).length) return next(400);
+      const { price } = req.body;
+      if (price !== undefined && !parseFloat(price)) return next(400);
+      const updatedProduct = await Product.findByIdAndUpdate(req.params.productId, req.body, {
+        new: true,
+      });
+      return resp.json(updatedProduct);
+    } catch (error) {
+      next(error);
+    }
   },
   deleteProductById: async (req, resp, next) => {
-    if (!idValidation(req.params.productId)) return next(404);
-    const product = await Product.findOneAndDelete({ _id: req.params.productId });
-    if (!product) return next(404);
-    return resp.json(product);
+    try {
+      if (!idValidation(req.params.productId)) return next(404);
+      const product = await Product.findOneAndDelete({ _id: req.params.productId });
+      if (!product) return next(404);
+      return resp.json(product);
+    } catch (error) {
+      next(error);
+    }
   },
 };
